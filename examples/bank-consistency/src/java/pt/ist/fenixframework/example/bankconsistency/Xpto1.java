f624d7c2fc0d86c0e9e0cbcfde07b3463d5d6a7f
João Neves <JoaoRoxoNeves@ist.utl.pt>
2012-04-20 16:08:38 +0100

    [Consistency Predicates] Bugfix: Corrected a problem while detecting already existing instances of new domain classes inside a hierarchy.

---
+++ examples/bank-consistency/src/java/pt/ist/fenixframework/example/bankconsistency/Xpto1.java
@@ -0,0 +1,9 @@
+package pt.ist.fenixframework.example.bankconsistency;
+
+public class Xpto1 extends Xpto1_Base {
+    
+    public  Xpto1() {
+        super();
+    }
+    
+}
---
+++ examples/bank-consistency/src/java/pt/ist/fenixframework/example/bankconsistency/Xpto2.java
@@ -0,0 +1,9 @@
+package pt.ist.fenixframework.example.bankconsistency;
+
+public class Xpto2 extends Xpto2_Base {
+    
+    public  Xpto2() {
+        super();
+    }
+    
+}
