2179a0d87d93046a792ae056d1250f033949f11a
João Neves <JoaoRoxoNeves@ist.utl.pt>
2012-09-12 15:27:56 +0100

    [Consistency Predicates] Minor Change: Removed the dependency to the javac ClassSymbol and MethodSymbol from the ConsistencyPredicateAnnotationProcessor, and updated the source version to Java 7.

--- fenix-framework-core/src/main/java/pt/ist/fenixframework/pstm/consistencyPredicates/ConsistencyPredicateAnnotationProcessor.java
+++ fenix-framework-core/src/main/java/pt/ist/fenixframework/pstm/consistencyPredicates/ConsistencyPredicateAnnotationProcessor.java
@@ -7,12 +7,11 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeKind;
-
-import com.sun.tools.javac.code.Symbol.ClassSymbol;
-import com.sun.tools.javac.code.Symbol.MethodSymbol;
 
 
 /**
@@ -20,33 +19,32 @@ import com.sun.tools.javac.code.Symbol.MethodSymbol;
  * primitive boolean value, and are public, protected, or private. Otherwise,
  * throws an <code>Error</code> during the compilation.
  */
-@SupportedSourceVersion(SourceVersion.RELEASE_6)
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
 @SupportedAnnotationTypes({ "pt.ist.fenixframework.pstm.consistencyPredicates.ConsistencyPredicate",
 	"jvstm.cps.ConsistencyPredicate" })
 public class ConsistencyPredicateAnnotationProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-	final Set<MethodSymbol> elements = (Set<MethodSymbol>) roundEnv.getElementsAnnotatedWith(ConsistencyPredicate.class);
-	elements.addAll((Set<MethodSymbol>) roundEnv.getElementsAnnotatedWith(jvstm.cps.ConsistencyPredicate.class));
+	final Set<Element> elements = (Set<Element>) roundEnv.getElementsAnnotatedWith(ConsistencyPredicate.class);
+	elements.addAll(roundEnv.getElementsAnnotatedWith(jvstm.cps.ConsistencyPredicate.class));
 
-	for (MethodSymbol method : elements) {
-	    ClassSymbol classSymbol = (ClassSymbol) method.getEnclosingElement();
-	    method.getModifiers();
-	    if (!method.getParameters().isEmpty()) {
+	for (Element method : elements) {
+	    TypeElement classSymbol = (TypeElement) method.getEnclosingElement();
+	    ExecutableElement executableMethod = (ExecutableElement) method;
+	    if (!executableMethod.getParameters().isEmpty()) {
 		throw new Error("Consistency Predicates cannot have parameters - " + classSymbol.getQualifiedName() + "."
-			+ method.getQualifiedName() + "()");
+			+ executableMethod.getSimpleName() + "()");
 	    }
-	    if (!method.getReturnType().getKind().equals(TypeKind.BOOLEAN)) {
+	    if (!executableMethod.getReturnType().getKind().equals(TypeKind.BOOLEAN)) {
 		throw new Error("Consistency Predicates must return a primitive boolean value - "
-			+ classSymbol.getQualifiedName()
-			+ "."
-			+ method.getQualifiedName() + "()");
+			+ classSymbol.getQualifiedName() + "." + executableMethod.getSimpleName() + "()");
 	    }
-	    if (!method.getModifiers().contains(Modifier.PUBLIC) && !method.getModifiers().contains(Modifier.PRIVATE)
-		    && !method.getModifiers().contains(Modifier.PROTECTED)) {
+	    if (!executableMethod.getModifiers().contains(Modifier.PUBLIC)
+		    && !executableMethod.getModifiers().contains(Modifier.PRIVATE)
+		    && !executableMethod.getModifiers().contains(Modifier.PROTECTED)) {
 		throw new Error("Consistency Predicates must be private, protected or public - " + classSymbol.getQualifiedName()
-			+ "." + method.getQualifiedName() + "()");
+			+ "." + executableMethod.getSimpleName() + "()");
 	    }
 	}
 
